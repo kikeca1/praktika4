f09eb9f9d2fdcafc163161915e0a7070159c3cb1
Your Name <you@example.com>
2024-07-10 11:24:34 +0300

    podobrenie

--- snake_game_dancho.py
+++ snake_game_dancho.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+import os
 
 pygame.init()
 
@@ -13,21 +14,22 @@ dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game')
 
 # Зареждане на задния план (background)
-background_img = pygame.image.load('C:/Users/User/Desktop/praktika4/background.png')
+background_img = pygame.image.load('C:/Users/User/Desktop/Snake Game/praktika4/background.png')
 background_img = pygame.transform.scale(background_img, (dis_width, dis_height))
 
 clock = pygame.time.Clock()
-snake_block = 30  # Увеличаваме размера на блока на змията
-snake_speed = 10  # Намаляване на скоростта на змийката
+snake_block = 30  # Размер на блока на змията
+normal_speed = 10  # Скорост за нормалното ниво
+hard_speed = 15  # Скорост за трудното ниво
 
-font_style = pygame.font.SysFont("bahnschrift", 25)  # Намаляване на размера на шрифта за съобщенията
+font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
 # Зареждане на изображения за главата на змията в различни посоки
-snake_head_up = pygame.image.load('C:/Users/User/Desktop/praktika4/snake_head_up.png')
-snake_head_down = pygame.image.load('C:/Users/User/Desktop/praktika4/snake_head_down.png')
-snake_head_left = pygame.image.load('C:/Users/User/Desktop/praktika4/snake_head_left.png')
-snake_head_right = pygame.image.load('C:/Users/User/Desktop/praktika4/snake_head_right.png')
+snake_head_up = pygame.image.load('C:/Users/User/Desktop/Snake Game/praktika4/snake_head_up.png')
+snake_head_down = pygame.image.load('C:/Users/User/Desktop/Snake Game/praktika4/snake_head_down.png')
+snake_head_left = pygame.image.load('C:/Users/User/Desktop/Snake Game/praktika4/snake_head_left.png')
+snake_head_right = pygame.image.load('C:/Users/User/Desktop/Snake Game/praktika4/snake_head_right.png')
 
 # Преоразмеряване на изображенията на главата на змията
 snake_head_up = pygame.transform.scale(snake_head_up, (snake_block, snake_block))
@@ -36,12 +38,32 @@ snake_head_left = pygame.transform.scale(snake_head_left, (snake_block, snake_bl
 snake_head_right = pygame.transform.scale(snake_head_right, (snake_block, snake_block))
 
 # Зареждане на изображението на ябълката
-apple_img = pygame.image.load('C:/Users/User/Desktop/praktika4/apple.png')
+apple_img = pygame.image.load('C:/Users/User/Desktop/Snake Game/praktika4/apple.png')
 apple_img = pygame.transform.scale(apple_img, (snake_block, snake_block))
 
-def score_display(score):
+# Зареждане на фоновата музика
+pygame.mixer.music.load('C:/Users/User/Desktop/Snake Game/praktika4/music.mp3')
+pygame.mixer.music.play(-1)  # Пускане на музиката на повторение
+
+# Проверка и създаване на файл за най-добрите резултати
+high_score_file = 'high_score.txt'
+if not os.path.exists(high_score_file):
+    with open(high_score_file, 'w') as f:
+        f.write('0')
+
+def get_high_score():
+    with open(high_score_file, 'r') as f:
+        return int(f.read().strip())
+
+def save_high_score(score):
+    with open(high_score_file, 'w') as f:
+        f.write(str(score))
+
+def score_display(score, high_score):
     value = score_font.render("Вашият резултат: " + str(score), True, (255, 255, 102))
+    high_score_value = score_font.render("Най-добър резултат: " + str(high_score), True, (255, 255, 102))
     dis.blit(value, [10, 10])
+    dis.blit(high_score_value, [10, 50])
 
 def our_snake(snake_block, snake_list, direction):
     for i, (x, y) in enumerate(snake_list):
@@ -68,7 +90,7 @@ def is_collision(apple_x, apple_y, snake_x, snake_y):
             return True
     return False
 
-def gameLoop():  # създаване на функция
+def gameLoop(snake_speed):  # Приема се скоростта като параметър
     game_over = False
     game_close = False
 
@@ -85,6 +107,8 @@ def gameLoop():  # създаване на функция
     foodx = round(random.randrange(0, dis_width - snake_block) / 30.0) * 30.0
     foody = round(random.randrange(0, dis_height - snake_block) / 30.0) * 30.0
 
+    high_score = get_high_score()
+
     while not game_over:
 
         while game_close == True:
@@ -98,7 +122,7 @@ def gameLoop():  # създаване на функция
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        gameLoop()
+                        gameLoop(snake_speed)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -149,20 +173,26 @@ def gameLoop():  # създаване на функция
             foodx = round(random.randrange(0, dis_width - snake_block) / 30.0) * 30.0
             foody = round(random.randrange(0, dis_height - snake_block) / 30.0) * 30.0
             Length_of_snake += 1
+            if Length_of_snake > high_score:
+                high_score = Length_of_snake  # Актуализиране на най-добър резултат
+                save_high_score(high_score)
         
-        # Добавяне на главата на змията в началото на списъка
+        # Добавяне на главата на змията към началото на списъка с позиции
         snake_List.insert(0, snake_Head)
-        
-        # Удължаване на змията
         if len(snake_List) > Length_of_snake:
-            del snake_List[-1]
+            snake_List.pop()
 
-        for i, (x, y) in enumerate(snake_List[:-1]):
-            if i != 0 and (x, y) == (x1, y1):  # Проверка за колизия с тялото на змията, без главата
+        # Проверка за колизия със собственото тяло
+        for segment in snake_List[1:]:
+            if segment == snake_Head:
                 game_close = True
 
+        # Показване на змията
         our_snake(snake_block, snake_List, direction)
-        score_display(Length_of_snake - 1)
+        
+        # Показване на текущ резултат и най-добър резултат
+        score_display(Length_of_snake - 1, high_score)
+        
         pygame.display.update()
 
         clock.tick(snake_speed)
@@ -170,4 +200,21 @@ def gameLoop():  # създаване на функция
     pygame.quit()
     quit()
 
-gameLoop()
+def show_difficulty_selection():
+    difficulty_selected = False
+
+    while not difficulty_selected:
+        dis.blit(background_img, (0, 0))
+        message("Натиснете 1 за нормално ниво, 2 за трудно ниво", (255, 255, 255))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    gameLoop(normal_speed)  # Започва играта с нормална скорост
+                    difficulty_selected = True
+                elif event.key == pygame.K_2:
+                    gameLoop(hard_speed)  # Започва играта с по-висока скорост
+                    difficulty_selected = True
+
+show_difficulty_selection()
